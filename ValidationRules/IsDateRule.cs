e1ee9ed84718f717c7993845049fab4d7f6dfbb7
MKdea <misha212032@gmail.com>
2025-11-28 22:01:39 +0500

    Добавление валидации даты

--- ValidationRules/IsDateRule.cs
+++ ValidationRules/IsDateRule.cs
@@ -19,6 +19,11 @@ namespace pr7_trpo_1_KMA.ValidationRules
                 return new ValidationResult(false, "Необходимо ввести дату");
             }
 
+            if (dateValue < new DateTime(1990, 01, 01) || dateValue > DateTime.Now)
+            {
+                return new ValidationResult(false, "Некоректная дата");
+            }
+
             return ValidationResult.ValidResult;
         }
     }
